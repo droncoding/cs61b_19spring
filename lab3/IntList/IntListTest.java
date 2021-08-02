85e10390b822e69d1f12b82f5f08d40922f94e88
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-02 23:03:42 +0800

    Lab3 completed

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -69,5 +69,13 @@ public class IntListTest {
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+    @Test
+    public void testReverse(){
+        IntList L = IntList.of(1,2,3);
+        IntList res = IntList.reverse(L);
+        assertNotNull(res);
+        assertNotEquals(IntList.of(1,2,3),L);
+        assertEquals(IntList.of(3,2,1),res);
+    }
 
 }
